b71a990fd525d87b14905732ac0d7e6209cf790d
mathieu <mathieu.delobelle@gmail.com>
2022-06-29 22:45:59 +0200

    null type checking

--- src/fileClass/FileClassAttributeModal.ts
+++ src/fileClass/FileClassAttributeModal.ts
@@ -1,31 +1,31 @@
-import {App, DropdownComponent, Modal, TextComponent, ButtonComponent, ExtraButtonComponent, TextAreaComponent} from "obsidian"
-import {FileClassAttribute, types} from "src/fileClass/fileClassAttribute"
-import {FileClass} from "src/fileClass/fileClass"
+import { App, DropdownComponent, Modal, TextComponent, ButtonComponent, ExtraButtonComponent, TextAreaComponent } from "obsidian"
+import { FileClassAttribute, types } from "src/fileClass/fileClassAttribute"
+import { FileClass } from "src/fileClass/fileClass"
 import { stringify } from "querystring"
 
 export default class FileClassAttributeModal extends Modal {
 
-    attr: FileClassAttribute
+    attr?: FileClassAttribute
     fileClass: FileClass
     type: string = "input"
     options: string[] = []
     name: string = ""
 
-    constructor(app: App, fileClass: FileClass, attr?: FileClassAttribute){
+    constructor(app: App, fileClass: FileClass, attr?: FileClassAttribute) {
         super(app)
         this.attr = attr
         this.fileClass = fileClass
-        if(this.attr){
+        if (this.attr) {
             this.type = this.attr.type || "input"
             this.options = this.attr.options
             this.name = this.attr.name
         }
     }
 
-    onOpen(){
+    onOpen() {
         //title
         this.titleEl.setText(this.attr ? `Manage ${this.attr.name}` : `Create a new attribute for ${this.fileClass.name}`)
-        
+
         //name input
         const nameInputContainer = this.contentEl.createDiv()
         nameInputContainer.setText("name")
@@ -43,21 +43,21 @@ export default class FileClassAttributeModal extends Modal {
         })
 
         // type select
-        const typeSelectContainer = this.contentEl.createDiv({cls: 'frontmatter-value-selector-container'})
-        const typeSelectLabel = typeSelectContainer.createDiv({cls: 'frontmatter-value-selector-inline-label'})
+        const typeSelectContainer = this.contentEl.createDiv({ cls: 'frontmatter-value-selector-container' })
+        const typeSelectLabel = typeSelectContainer.createDiv({ cls: 'frontmatter-value-selector-inline-label' })
         typeSelectLabel.setText("will: ")
-        const typeSelectDropDown = typeSelectContainer.createDiv({cls: 'frontmatter-value-selector-toggler'})
+        const typeSelectDropDown = typeSelectContainer.createDiv({ cls: 'frontmatter-value-selector-toggler' })
         const typeSelect = new DropdownComponent(typeSelectDropDown)
         Object.keys(types).forEach(key => {
             typeSelect.addOption(key, types[key])
         })
-        if(this.attr){
+        if (this.attr) {
             typeSelect.setValue(this.type)
         }
 
         // options input
-        const optionsInputContainer = this.contentEl.createDiv({cls: 'frontmatter-value-selector-container'})
-        const optionsInputLabel = optionsInputContainer.createDiv({cls: 'frontmatter-value-selector-inline-label-top'})
+        const optionsInputContainer = this.contentEl.createDiv({ cls: 'frontmatter-value-selector-container' })
+        const optionsInputLabel = optionsInputContainer.createDiv({ cls: 'frontmatter-value-selector-inline-label-top' })
         optionsInputLabel.setText("Values")
         const optionsInput = new TextAreaComponent(optionsInputContainer)
         optionsInput.inputEl.rows = 3
@@ -71,28 +71,28 @@ export default class FileClassAttributeModal extends Modal {
             this.type = type
         })
         optionsInput.onChange(value => this.options = value.split(",").map(item => item.trim()))
-        nameInput.onChange(value => {this.name = value; attrName.setText(`<${value}>`)})
+        nameInput.onChange(value => { this.name = value; attrName.setText(`<${value}>`) })
 
         // footer buttons
-        const footer = this.contentEl.createDiv({cls: "frontmatter-value-grid-footer"})
+        const footer = this.contentEl.createDiv({ cls: "frontmatter-value-grid-footer" })
         const saveButton = new ButtonComponent(footer)
         saveButton.setIcon("checkmark")
         saveButton.onClick(() => {
             this.fileClass.updateAttribute(this.type, this.options, this.name, this.attr)
             this.close()
         })
-        if(this.attr){
+        if (this.attr) {
             const removeButton = new ButtonComponent(footer)
             removeButton.setIcon("trash")
             removeButton.onClick(() => {
                 const confirmModal = new Modal(this.app)
                 confirmModal.titleEl.setText("Please confirm")
-                confirmModal.contentEl.createDiv().setText(`Do you really want to remove ${this.attr.name} attribute from ${this.fileClass.name}?`)
-                const confirmFooter = confirmModal.contentEl.createDiv({cls: "frontmatter-value-grid-footer"})
+                confirmModal.contentEl.createDiv().setText(`Do you really want to remove ${this.attr?.name} attribute from ${this.fileClass.name}?`)
+                const confirmFooter = confirmModal.contentEl.createDiv({ cls: "frontmatter-value-grid-footer" })
                 const confirmButton = new ButtonComponent(confirmFooter)
                 confirmButton.setIcon("checkmark")
                 confirmButton.onClick(() => {
-                    this.fileClass.removeAttribute(this.attr)
+                    if (this.attr) this.fileClass.removeAttribute(this.attr)
                     confirmModal.close()
                     this.close()
                 })
