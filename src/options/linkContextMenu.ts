e522b91b7fbe5abeea9b83651ecd15885baf4d9b
SlRvb <slrvb17@gmail.com>
2022-07-21 20:22:54 -0700

    Display Fields is False: Add Field & Field Options

--- src/options/linkContextMenu.ts
+++ src/options/linkContextMenu.ts
@@ -2,6 +2,7 @@ import { TFile } from "obsidian";
 import MetadataMenu from "main";
 import OptionsList from "src/options/OptionsList";
 import FileClassAttributeSelectModal from "src/fileClass/FileClassAttributeSelectModal";
+import NoteFieldsCommandsModal from "./NoteFieldsCommandsModal";
 
 export default class linkContextMenu {
 	private plugin: MetadataMenu;
@@ -16,14 +17,18 @@ export default class linkContextMenu {
 	private createContextMenu(): void {
 		this.plugin.registerEvent(
 			this.plugin.app.workspace.on('file-menu', (menu, abstractFile, source) => {
-				if (this.plugin.settings.displayFieldsInContextMenu && (
+				if ((
 					source === "link-context-menu" ||
 					source === "calendar-context-menu" ||
 					source === 'pane-more-options' ||
 					source === 'file-explorer-context-menu')) {
-					const file = this.plugin.app.vault.getAbstractFileByPath(abstractFile.path)
+
+					const file = this.plugin.app.vault.getAbstractFileByPath(abstractFile.path);
+
 					if (file instanceof TFile && file.extension === 'md') {
 						this.file = file;
+						
+						//If fileClass
 						if (file.parent.path + "/" == this.plugin.settings.classFilesPath) {
 							menu.addSeparator();
 							menu.addItem((item) => {
@@ -35,8 +40,30 @@ export default class linkContextMenu {
 								});
 							});
 						} else {
-							this.optionsList = new OptionsList(this.plugin, this.file, menu);
-							this.optionsList.createExtraOptionList();
+							//If displayFieldsInContextMenu true, show all fields in note
+							if(this.plugin.settings.displayFieldsInContextMenu) {
+								this.optionsList = new OptionsList(this.plugin, this.file, menu);
+								this.optionsList.createExtraOptionList();
+							} else {
+
+								//New Field
+								if (file instanceof TFile && file.extension === 'md') {
+									this.file = file;
+									this.optionsList = new OptionsList(this.plugin, this.file, menu);
+									this.optionsList.addSectionSelectModalOption();
+								}
+
+								//Field Options
+								menu.addItem((item) => {
+									item.setIcon("bullet-list"),
+									item.setTitle(`Field Options`),
+									item.onClick((evt) => {
+										const fieldOptions = new NoteFieldsCommandsModal(app, this.plugin, file);
+										fieldOptions.open();
+									})
+									item.setSection("target-metadata");
+								})
+							}
 						};
 					};
 				};
