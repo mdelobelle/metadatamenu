191ca2ea54d886aed64346dc2a7e5ed73eb359d9
mathieu <mathieu.delobelle@gmail.com>
2022-07-24 23:15:56 +0200

    dv table field modifier

--- src/MetadataMenuApi.ts
+++ src/MetadataMenuApi.ts
@@ -2,10 +2,13 @@ import { App, TFile } from "obsidian"
 import MetadataMenu from "main"
 import { getValues } from "./commands/getValues";
 import { replaceValues } from "./commands/replaceValues";
+import { fieldWithMenu } from "./commands/fieldWithMenu";
+
 
 export interface IMetadataMenuApi {
     getValues: (app: App, file: TFile, attribute: string) => Promise<string[]>;
     replaceValues: (app: App, file: TFile, attribute: string, input: string) => Promise<void>;
+    fieldWithMenu: (dv: any, p: any, fieldName: string, attrs?: { cls: string, attr: Record<string, string> }) => HTMLElement;
 }
 
 export class MetadataMenuApi {
@@ -15,7 +18,8 @@ export class MetadataMenuApi {
     public make(): IMetadataMenuApi {
         return {
             getValues: this.getValues(),
-            replaceValues: this.replaceValues()
+            replaceValues: this.replaceValues(),
+            fieldWithMenu: this.fieldWithMenu()
         };
     }
 
@@ -25,5 +29,9 @@ export class MetadataMenuApi {
 
     private replaceValues(): (app: App, file: TFile, attribute: string, input: string) => Promise<void> {
         return async (app: App, file: TFile, attribute: string, input: string) => replaceValues(app, file, attribute, input)
+    }
+
+    private fieldWithMenu(): (dv: any, p: any, fieldName: string, attrs?: { cls: string, attr: Record<string, string> }) => HTMLElement {
+        return (dv: any, p: any, fieldName: string, attrs?: { cls: string, attr: Record<string, string> }) => fieldWithMenu(this.plugin, dv, p, fieldName, attrs)
     }
 }
