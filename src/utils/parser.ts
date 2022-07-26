2bd3b8e0d75c8a889576d459d391d9669f4bbeba
zackdn <zackdn@gmail.com>
2022-07-26 14:11:45 -0400

    Update parser.ts
    Updated regex to support multiple dataview components on one line

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,5 +1,5 @@
 export const fieldComponents = ['startStyle', 'attribute', 'endStyle', 'beforeSeparatorSpacer', 'afterSeparatorSpacer', 'values']
 
-var genericFieldRegex = "(?<startStyle>[_\\*~`\\[\\(]*)(?<attribute>[0-9\\w\\p{Letter}\\p{Emoji_Presentation}][-0-9\\w\\p{Letter}\\p{Emoji_Presentation}\\s]*)(?<endStyle>[_\\*~`]*)(?<beforeSeparatorSpacer>\\s*)";
+export const genericFieldRegex = "(?<startStyle>[_\\*~`\\[\\(]*)(?<attribute>[0-9\\w\\p{Letter}\\p{Emoji_Presentation}][-0-9\\w\\p{Letter}\\p{Emoji_Presentation}\\s]*)(?<endStyle>[_\\*~`]*)(?<beforeSeparatorSpacer>\\s*)";
 
-var inlineFieldRegex = (attribute) => new RegExp(`(?<startStyle>[_*~\`\[\(]*)(?<attribute>${attribute})(?<endStyle>[_*~\`]*)(?<beforeSeparatorSpacer>\\s*)::(?<afterSeparatorSpacer>\\s*)(?<values>[^\]\)\r]+[\]\)]?)`, "u");
+export const inlineFieldRegex = (attribute) => new RegExp(`(?<startStyle>[_*~\`\[\(]*)(?<attribute>${attribute})(?<endStyle>[_*~\`]*)(?<beforeSeparatorSpacer>\\s*)::(?<afterSeparatorSpacer>\\s*)(?<values>[^\]\)\r]+[\]\)]?)`, "u");
