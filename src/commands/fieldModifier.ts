e95dd4c9eb87e762af2f3857e33d5659508ea911
mathieu <mathieu.delobelle@gmail.com>
2022-09-10 18:31:44 +0200

    Update fieldModifier.ts

--- src/commands/fieldModifier.ts
+++ src/commands/fieldModifier.ts
@@ -5,6 +5,23 @@ import { FieldManager } from "src/types/fieldTypes";
 import { FieldManager as F } from "src/fields/FieldManager";
 import chooseSectionModal from "src/optionModals/chooseSectionModal";
 import { setIcon, TFile } from "obsidian";
+import FileClassQuery from "src/fileClass/FileClassQuery";
+
+
+function getQueryFileClassForFields(plugin: MetadataMenu, file: TFile): FileClass | undefined {
+    let fileClassForFields = false;
+    let fileClass: FileClass | undefined;
+    const fileClassQueries = plugin.settings.fileClassQueries.map(fcq => fcq)
+    while (!fileClassForFields && fileClassQueries.length > 0) {
+        const fileClassQuery = new FileClassQuery(plugin);
+        Object.assign(fileClassQuery, fileClassQueries.pop() as FileClassQuery)
+        if (fileClassQuery.matchFile(file)) {
+            fileClassForFields = true;
+            fileClass = FileClass.createFileClass(plugin, fileClassQuery.fileClassName)
+        }
+    }
+    return fileClass
+}
 
 export async function fieldModifier(plugin: MetadataMenu, dv: any, p: any, fieldName: string, attrs?: { cls?: string, attr?: Record<string, string>, options?: Record<string, string> }): Promise<HTMLElement> {
 
@@ -28,8 +45,26 @@ export async function fieldModifier(plugin: MetadataMenu, dv: any, p: any, field
                 const file = plugin.app.vault.getAbstractFileByPath(p.file.path)
                 if (file instanceof TFile && file.extension == "md") {
                     const fileClassAlias = plugin.settings.fileClassAlias;
-                    if (p[fileClassAlias] || plugin.settings.globalFileClass) {
-                        const fileClassName = p[fileClassAlias] || plugin.settings.globalFileClass // inner fileClass has the priority over global fileClass
+                    const queryFileClass = getQueryFileClassForFields(plugin, file)
+                    if (p[fileClassAlias]) {
+                        const fileClassName = p[fileClassAlias]
+                        const fileClass = FileClass.createFileClass(plugin, fileClassName);
+                        if (attrs?.options?.inFrontmatter && plugin.app.metadataCache.getCache(file.path)?.frontmatter) {
+                            const lineNumber = plugin.app.metadataCache.getCache(file.path)!.frontmatter!.position.end.line - 1
+                            F.openFieldModal(plugin, file, fieldName, "", lineNumber, true, false, fileClass)
+                        } else {
+                            new chooseSectionModal(plugin, file, fileClass, fieldName).open();
+                        }
+                    } else if (queryFileClass) {
+                        if (attrs?.options?.inFrontmatter && plugin.app.metadataCache.getCache(file.path)?.frontmatter) {
+                            const lineNumber = plugin.app.metadataCache.getCache(file.path)!.frontmatter!.position.end.line - 1
+                            F.openFieldModal(plugin, file, fieldName, "", lineNumber, true, false, queryFileClass)
+                        } else {
+                            new chooseSectionModal(plugin, file, queryFileClass, fieldName).open();
+                        }
+
+                    } else if (plugin.settings.globalFileClass) {
+                        const fileClassName = plugin.settings.globalFileClass
                         const fileClass = FileClass.createFileClass(plugin, fileClassName);
                         if (attrs?.options?.inFrontmatter && plugin.app.metadataCache.getCache(file.path)?.frontmatter) {
                             const lineNumber = plugin.app.metadataCache.getCache(file.path)!.frontmatter!.position.end.line - 1
