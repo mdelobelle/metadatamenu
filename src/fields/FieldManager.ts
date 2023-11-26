9050a12e4c769a409d40b8166789f81c89eeaa1a
mathieu <mathieu.delobelle@gmail.com>
2023-11-26 15:26:20 +0100

    Update FieldManager.ts
    enable overiding field name

--- src/fields/FieldManager.ts
+++ src/fields/FieldManager.ts
@@ -69,8 +69,10 @@ export abstract class FieldManager {
             error = true;
         };
         if (this.field.fileClassName) {
-            const fields = this.plugin.fieldIndex.fileClassesFields.get(this.field.fileClassName)?.filter(_f => _f.path === this.field.path && _f.id !== this.field.id)
+            const fields = this.plugin.fieldIndex.fileClassesFields
+                .get(this.field.fileClassName)?.filter(_f => _f.path === this.field.path && _f.id !== this.field.id && _f.fileClassName === this.field.fileClassName)
             if (fields?.map(_f => _f.name).includes(this.field.name)) {
+                console.log(this.field)
                 FieldSettingsModal.setValidationError(
                     textInput,
                     `There is already a field with this name for this path in this ${this.plugin.settings.fileClassAlias}. Please choose another name`
